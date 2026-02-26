9f8bba2f36615846b91c795ebe0fa339cfea634e
XHub157 <146161578+XHub157@users.noreply.github.com>
2026-02-26 08:12:14 +0300

    Delete diplom.j38202257.myjino.ru directory

--- diplom.j38202257.myjino.ru/db.php
+++
@@ -1,5 +0,0 @@
-<?php
-$mysql = @new Mysqli('localhost', '047582029_diplom', 'Diplom_41', 'j38202257_diplom');
-if($mysql->connecterrno) exit('Ошибка подключения к БД');
-$mysql-set_charset('utf8');
-?>
